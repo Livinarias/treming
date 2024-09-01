d7845d0cd296a3473aab32226edc22ee59d5d284
Livingston <livin.arias88@gmail.com>
2024-08-31 22:12:16 -0500

    feat: :hammer: consume to api and logic to calculate price and date

--- addons/package_calculation/api/api_georef.py
+++ addons/package_calculation/api/api_georef.py
@@ -9,19 +9,19 @@ class ApiGeorefTemplate(models.AbstractModel):
     _name = "api.georef.template"
     _description = "Api georef Template"
 
-    def get_countries(self, headers=None, params=None, api=""):
+    def get_countries(self, headers=None, params=None, url=""):
         try:
-            response = requests.get(api, headers=headers, params=params, timeout=10)
-            if response.status_code != 200:
+            response = requests.get(url, headers=headers, params=params, timeout=10)
+            if response.status_code == 200:
                 return response.json()
             logging.info(response.json())
         except requests.exceptions.HTTPError as errh:
             logging.error(errh.message)
 
-    def get_time_zone(self, headers=None, params=None, api=""):
+    def get_time_zone(self, headers=None, url=""):
         try:
-            response = requests.get(api, headers=headers, timeout=10)
-            if response.status_code != 200:
+            response = requests.get(url, headers=headers, timeout=10)
+            if response.status_code == 200:
                 return response.json()
             logging.info(response.json())
         except requests.exceptions.HTTPError as errh:
